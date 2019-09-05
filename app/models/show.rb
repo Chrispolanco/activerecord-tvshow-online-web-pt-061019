4b738febdef9e04b4bf4ce6b7486c0f4a53d713a
Christian Polanco <chrispolanco@sbcglobal.net>
2019-09-05 05:10:41 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,36 @@
+class Show < ActiveRecord::Base
+  
+  def highest_rating 
+    self.maximum(:rating)
+  end 
+  
+  
+  def most_popular_show 
+    self.where("rating = ?", self.higest_rating)
+  end 
+  
+  
+  def lowest_rating 
+    self.minimum(:rating)
+  end 
+  
+  
+  def least_popular_show 
+    self.where("rating =?", self.lowest_rating)
+  end 
+  
+  
+  def ratings_sum 
+    self.sum(:rating)
+  end 
+  
+  
+  def popular_shows 
+    self.where("rating > 5")
+  end 
+  
+  def shows_by_alphabetical_order 
+    self.order(:name)
+  end 
+  
+end 
